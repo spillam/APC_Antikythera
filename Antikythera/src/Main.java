afbe8a802010bf0a6573efa3d50c307990dac9b8
Ethan Rajkumar <144576900+EthanRajkumar@users.noreply.github.com>
2025-06-05 16:05:17 -0400

    Setup Initial Project
    Setup java project files w/ SQL integration

---
+++ Antikythera/src/Main.java
@@ -0,0 +1,31 @@
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Main {
+
+    //slime
+
+    public static void SQLMode() {
+        // connection string
+        var url = "jdbc:sqlite:Data/assignment3.db";
+        Scanner scanner = new Scanner(System.in);
+
+        // TO-DO: User Interface
+
+        try (var conn = DriverManager.getConnection(url)) {
+            Statement statement = conn.createStatement();
+
+            //Insert SQL working here
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        SQLMode();
+    }
+}
