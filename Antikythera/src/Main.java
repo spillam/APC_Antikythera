74351324b6287db49c6ade40fae037467ccf1662
Christopher Le <lec5@wit.edu>
2025-06-11 15:32:46 -0400

    Code for setting up database, with latest Solar and Lunar eclipses added

--- Antikythera/src/Main.java
+++ Antikythera/src/Main.java
@@ -10,15 +10,44 @@ public class Main {
 
     public static void SQLMode() {
         // connection string
-        var url = "jdbc:sqlite:Antikythera/Data/assignment3.db";
+        var url = "jdbc:sqlite:Antikythera/Data/antikythera.db";
         Scanner scanner = new Scanner(System.in);
+
+        //create table for Lunar eclipse
+        var lunarEclipse = "CREATE TABLE IF NOT EXISTS LUNARECLIPSE ("
+                + " DATE txt PRIMARY KEY,"
+                + " TD txt NOT NULL,"
+                + " TYPE txt NOT NULL,"
+                + " SAROS INTEGER NOT NULL,"
+                + " MAGNITUDE REAL NOT NULL,"
+                + " CENTRALDURATION INTEGER NOT NULL,"
+                + " VISIBILITY txt NOT NULL"
+                + ");";
+
+        var solarEclipse = "CREATE TABLE IF NOT EXISTS SOLARECLIPSE ("
+                + " DATE txt PRIMARY KEY,"
+                + " TD txt NOT NULL,"
+                + " TYPE txt NOT NULL,"
+                + " SAROS INTEGER NOT NULL,"
+                + " MAGNITUDE REAL NOT NULL,"
+                + " CENTRALDURATION INTEGER NOT NULL,"
+                + " VISIBILITY txt NOT NULL"
+                + ");";
 
         // TO-DO: User Interface
 
         try (var conn = DriverManager.getConnection(url)) {
             Statement statement = conn.createStatement();
 
+            statement.execute(lunarEclipse);
+            statement.execute(solarEclipse);
+
             //Insert SQL working here
+
+            String lunEcl = "INSERT INTO LUNARECLIPSE VALUES ('2025 March 14', '6:59:56 AM', 'Total', 123, 1.178, 65, 'Pacific, Americas, W Europe, W Africa')";
+            statement.executeUpdate(lunEcl);
+            String solEcl = "INSERT INTO SOLARECLIPSE VALUES ('2025 March 29', '10:48:36 AM', 'Partial', 149, 0.938, 0, 'NW Africa, Europe, N Russia')";
+            statement.executeUpdate(solEcl);
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
