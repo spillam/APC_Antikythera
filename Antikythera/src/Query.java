40b97bb144fe3aae96574701c6e58102bbec2e9e
Christopher Le <lec5@wit.edu>
2025-06-25 22:50:32 -0400

    Set up Query class intended to hold querying functions. Functions have return type int and are empty for now.

---
+++ Antikythera/src/Query.java
@@ -0,0 +1,27 @@
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Query {
+
+    int getDay() {      //pass in eclipse object as parameter?
+        int day = 0;
+        //insert querying
+        return day;
+    }
+
+    int getMonth() {    //pass in eclipse object as parameter?
+        int month = 0;
+        //insert querying
+        return month;
+    }
+
+    int getYear() {    //pass in eclipse object as parameter?
+        int year = 0;
+        //insert querying
+        return year;
+    }
+
+}
